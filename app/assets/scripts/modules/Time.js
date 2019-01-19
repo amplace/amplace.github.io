341e4fbdf47bfc7b9125438c4c93367674e7d335
Annie <Anniebelkin97@gmail.com>
2019-01-19 21:10:56 +0200

    Add color themes by the time of the day

--- app/assets/scripts/modules/Time.js
+++ app/assets/scripts/modules/Time.js
@@ -4,12 +4,19 @@ const Time = () => {
     const date = new Date();
     const time = date.getHours();
     const greet = $('#greeting');
+    const body = $('body');
 
-    if (time >= 5 && time <= 12) {
-        greet.html('<i class="fas fa-sun"></i> Good Morning');
-    } else if (time > 12 && time <= 5) {
-        greet.html('<i class="fas fa-cloud-sun"></i> Good Afternoon');
+    if (time >= 5 && time < 17) {
+        body.removeClass('night');
+        body.addClass('day');
+        if (time <= 12) {
+            greet.html('<i class="fas fa-sun"></i> Good Morning');
+        } else {
+            greet.html('<i class="fas fa-cloud-sun"></i> Good Afternoon');
+        }
     } else {
+        body.removeClass('day');
+        body.addClass('night');
         greet.html('<i class="fas fa-cloud-moon"></i> Good Evening');
     }
 
