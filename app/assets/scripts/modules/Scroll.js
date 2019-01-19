84b8bf7061f14fb6007d54f75440f254a3351908
Annie <Anniebelkin97@gmail.com>
2019-01-19 13:29:32 +0200

    Add counters animation and reveal on scroll

--- app/assets/scripts/modules/Scroll.js
+++ app/assets/scripts/modules/Scroll.js
@@ -15,8 +15,47 @@ const ScrollSpy = (thisMenu) => {
         let scrollPosition = document.documentElement.scrollTop || $('body').scrollTop;
         for (i in sections) {
             if (sections[i] <= scrollPosition + ($(window).height() / 2)) {
-                $('.navbar__link--active').removeClass('navbar__link--active');
-                $('#' + i + '-link').addClass('navbar__link--active');
+                let link = $('#' + i + '-link');
+                let block = $('#' + i + '-section');
+                let inside = $('#' + i + '-animate');
+                if (link) {
+                    $('.navbar__link--active').removeClass('navbar__link--active');
+                    link.addClass('navbar__link--active');
+                }
+
+                if (!block.hasClass('reveal')) {
+                    block.addClass('reveal');
+                    if (i === 'count') {
+                        $('.count-up').each(function () {
+                            $(this).prop('Counter', 0).animate({
+                                Counter: $(this).text()
+                            }, {
+                                duration: 2000,
+                                easing: 'swing',
+                                step: function (now) {
+                                    $(this).text(Math.ceil(now));
+                                }
+                            });
+                        });
+
+                        $('.count-down').each(function () {
+                            $(this).prop('Counter', 2019).animate({
+                                Counter: $(this).text()
+                            }, {
+                                duration: 1500,
+                                easing: 'swing',
+                                step: function (now) {
+                                    $(this).text(Math.ceil(now));
+                                }
+                            });
+                        });
+                    }
+                }
+
+                if (!inside.hasClass('animate')) {
+                    inside.addClass('animate');
+                }
+
             }
         }
     };
