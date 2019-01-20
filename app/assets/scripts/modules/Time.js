34ae1f68e232d3a6486c4957052708251f14234f
Annie <Anniebelkin97@gmail.com>
2019-01-20 23:47:49 +0200

    Add theme btn on navbar

--- app/assets/scripts/modules/Time.js
+++ app/assets/scripts/modules/Time.js
@@ -4,21 +4,39 @@ const Time = () => {
     const date = new Date();
     const time = date.getHours();
     const greet = $('#greeting');
+    const dayTime = $('.day-time');
     const body = $('body');
+    let isDay = true;
+
+    const updateColors = () => {
+        if (isDay) {
+            dayTime.removeClass('night');
+            dayTime.addClass('day');
+        } else {
+            dayTime.removeClass('day');
+            dayTime.addClass('night');
+        }
+    }
 
     if (time >= 5 && time < 17) {
-        body.removeClass('night');
-        body.addClass('day');
+        isDay = true;
+        updateColors();
+
         if (time <= 12) {
             greet.html('<i class="fas fa-sun"></i> Good Morning');
         } else {
             greet.html('<i class="fas fa-cloud-sun"></i> Good Afternoon');
         }
     } else {
-        body.removeClass('day');
-        body.addClass('night');
+        isDay = false;
+        updateColors();
         greet.html('<i class="fas fa-cloud-moon"></i> Good Evening');
     }
+
+    $('#day-time').on('click', function (e) {
+        isDay = !isDay;
+        updateColors();
+    });
 
 };
 
