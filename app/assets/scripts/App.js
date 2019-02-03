d264143f1828bcb58ad03bb1b785ce453513df30
Annie <Anniebelkin97@gmail.com>
2019-02-03 06:53:22 +0200

    Add portfolio section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,3 +6,9 @@ import Time from './modules/Time';
 const menu = new Menu();
 ScrollSpy(menu);
 Time();
+
+$(".thumbnail-container").click(function () {
+    let url = $(this).find('a')[0];
+    window.open(url, '_blank');
+    return false;
+});
